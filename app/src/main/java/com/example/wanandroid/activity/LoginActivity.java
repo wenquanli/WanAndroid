f10285c3b59dc6e8ae7e0a02bb61f9306273b145
Ned <740472039@qq.com>
2020-06-20 09:21:57 +0800

    准备切换分支 写login逻辑

---
+++ app/src/main/java/com/example/wanandroid/activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.wanandroid.activity;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.wanandroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
--- app/src/main/java/com/example/wanandroid/fragments/MeFragment.java
+++ app/src/main/java/com/example/wanandroid/fragments/MeFragment.java
@@ -1,22 +1,37 @@
 package com.example.wanandroid.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.wanandroid.R;
+import com.example.wanandroid.activity.LoginActivity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MeFragment extends androidx.fragment.app.Fragment {
+    Context context;
     public MeFragment() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_me,container,false);
+        View view = inflater.inflate(R.layout.fragment_me,container,false);
+        context = getActivity();
+        Button button = (Button)view.findViewById(R.id.button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
